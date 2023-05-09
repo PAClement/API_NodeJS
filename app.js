fd896b576a793a58bbc8c072bdf1641da0b5d613
PAClement <clementpaquentin@hotmail.fr>
2023-05-09 17:01:09 +0200

    add getAllStyle + bdd connection

--- app.js
+++ app.js
@@ -7,45 +7,66 @@ const app = express();
 const port = 8080;
 
 const pool = mariadb.createPool({
-    host: 'http://localhost:4001/',
+    host: 'localhost',
+    port: 4000,
     user:'root',
     password: 'rootpwd',
+    database: 'liveAddict',
     connectionLimit: 5
 });
 
 const schema = buildSchema(`
   type User {
     id: Int
-    email: String!
-    password: String!
+    email: String
+    password: String
   }
   
-  type Test {
-    id: Int
-    email: String!
-    password: String!
+  type Style{
+    idStyle: Int!
+    libelle: String
+    description: String
   }
-
+  
   type Query {
     user(id: Int): User!,
-    test(): Test!
+    getAllUser: [User]!,
+    getAllStyle: [Style]!
   }
 `)
 
 // Maps id to User object
 const usersFakeData = [
-    {id: 1, email:'a@a.a',password:'zaq1@WSX'},
-    {id: 2, email:'b@b.b',password:'ZAQ!2wsx'}
+    {id: 1, "email":'a@a.a',password:'zaq1@WSX'},
+    {id: 2, "email":'b@b.b',password:'ZAQ!2wsx'}
 ];
 
 // The root provides a resolver function for each API endpoint
 const resolver = {
     user(parent, args, contextValue, info) {
-        return usersFakeData.find((user) => usersFakeData.id === args.id);
+        return usersFakeData.find((user) => user.id === parent.id);
     },
-    test(parent, args, contextValue){
-        console.log(usersFakeData)
+    getAllUser(parent, args, contextValue){
         return usersFakeData
+    },
+    async getAllStyle(){
+        return await pool.getConnection()
+            .then(conn => {
+
+                return conn.query("SELECT * FROM Style")
+                    .then((rows) => {
+                        conn.end();
+                        return rows
+                    })
+                    .catch(err => {
+                        //handle error
+                        console.log(err);
+                        conn.end();
+                    })
+            }).catch(err => {
+                //not connected
+                console.log(err);
+            });
     }
 }
 
